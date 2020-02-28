aec06c66ddf723c48b4f10568254bf43f882ae59
Takuya Nakada <tky.play@gmail.com>
2020-02-29 00:13:20 +0900

    first

---
+++ Assets/Sample/MnistInference.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.ML.OnnxRuntime;
+using Microsoft.ML.OnnxRuntime.Tensors;
+using UnityEngine;
+
+public class MnistInference
+{
+    private readonly InferenceSession session;
+
+    public MnistInference(string modelPath)
+    {
+        session = new InferenceSession(modelPath);
+    }
+
+    //推論
+    //Mnistは28x28のfloat値(0~1)のinputで推論できる，左上が原点で右下に向かう座標系
+    public int Inference(float[] input_floats)
+    {
+        //推論する
+        var scores = InferenceOnnx(input_floats);
+
+        //最大のIndexを求める．Indexが推論した数字
+        var maxScore = float.MinValue;
+        int maxIndex = 0;
+        for (int i = 0; i < scores.Length; i++)
+        {
+            float score = scores[i];
+            if (maxScore < score)
+            {
+                maxScore = score;
+                maxIndex = i;
+            }
+        }
+
+        return maxIndex;
+    }
+
+    private float[] InferenceOnnx(float[] input)
+    {
+        var inputName = session.InputMetadata.First().Key;
+        var inputDim = session.InputMetadata.First().Value.Dimensions;
+        var inputTensor = new DenseTensor<float>(new System.Memory<float>(input), inputDim);
+
+        // OnnxRuntimeでの入力形式であるNamedOnnxValueを作成する
+        var inputOnnxValues = new List<NamedOnnxValue> {
+            NamedOnnxValue.CreateFromTensor (inputName, inputTensor)
+        };
+
+        // 推論を実行
+        var results = session.Run(inputOnnxValues);
+        var scores = results.First().AsTensor<float>().ToArray();
+
+        return scores;
+    }
+}
---
+++ Assets/Sample/Paint.cs
@@ -0,0 +1,82 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Paint : MonoBehaviour
+{
+    const int width = 28;
+    const int height = 28;
+
+    public MeshRenderer m;
+    public Button b;
+    public Text answer;
+    
+    private Texture2D t;
+    private Color[] buffer = new Color[width * height];
+    private float[] input = new float[width * height];
+    private MnistInference mnistInference;
+
+    private void Start()
+    {
+        mnistInference = new MnistInference(Application.dataPath + "/MLModel/model.onnx");
+        t = new Texture2D(width, height);
+        m.material.mainTexture = t;
+        ClearBuffer();
+    }
+
+    //４ピクセル塗る
+    private void Draw(Vector2Int p)
+    {
+        DrawBuffer(p.x    , p.y);
+        DrawBuffer(p.x + 1, p.y);
+        DrawBuffer(p.x + 1, p.y + 1);
+        DrawBuffer(p.x    , p.y + 1);
+    }
+
+    private void DrawBuffer(int x, int y)
+    {
+        if (x < 0 || width <= x || y < 0 || height <= y)
+        {
+            return;
+        }
+
+        buffer.SetValue(Color.white, x + width * y);
+    }
+
+    private void Update()
+    {
+        //クリック座標を白く塗りつぶす
+        if (Input.GetMouseButton(0))
+        {
+            var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out var hit, 100.0f))
+            {
+                Draw(Vector2Int.RoundToInt(hit.textureCoord * 28));
+            }
+
+            t.SetPixels(buffer);
+            t.Apply();
+        }
+
+        //塗りつぶした部分を1としてfloatの配列に代入
+        for (int i = 0; i < buffer.Length; i++)
+        {
+            input[i] = buffer[i].r;
+        }
+
+        //推論
+        var num = mnistInference.Inference(input);
+        answer.text = num.ToString();
+    }
+
+    //黒(0)で塗りつぶしてクリア
+    public void ClearBuffer()
+    {
+        for (int i = 0; i < buffer.Length; i++)
+        {
+            buffer[i] = Color.black;
+        }
+
+        t.SetPixels(buffer);
+        t.Apply();
+    }
+}
